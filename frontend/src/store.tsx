89cd5bdfcf10e85dd9a148fc087df7a550339f01
edidiesky <essienedidiong1000@gmail.com>
2024-01-22 05:42:56 -0800

    new fixes

--- frontend/src/store.tsx
+++ frontend/src/store.tsx
@@ -3,6 +3,7 @@ import authSlice from './features/auth/authSlice'
 import transactionSlice from './features/transaction/transactionSlice'
 import investmentsSlice from './features/investments/investmentsSlice'
 import depositSlice from './features/deposit/depositSlice'
+import roiSlice from './features/roi/roiSlice'
 // ...
 
 export const store = configureStore({
@@ -11,6 +12,7 @@ export const store = configureStore({
     transaction: transactionSlice,
     investments: investmentsSlice,
     deposit: depositSlice,
+    roi: roiSlice
   },
 })
 
