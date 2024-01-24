6c0c53c9bbbae1b3942912d6e2a197e41aa85e8d
edidiesky <essienedidiong1000@gmail.com>
2024-01-24 01:49:11 -0800

    new fixes

--- frontend/src/components/home/NewsLetter.tsx
+++ frontend/src/components/home/NewsLetter.tsx
@@ -5,9 +5,9 @@ import { BsCash } from "react-icons/bs";
 const NewsLetter = () => {
 
     return (
-        <NewsLetterStyles className="w-100 flex flex-col gap-4">
-            <div className="auto w-[85%] lg:w-[70%]  grid grid-cols-1 lg:grid-cols-3 items-center justify-center">
-                <div className="p-4 pt-4 w-100 flex items-start flex-col gap-2  border-b lg:border-b-0 pb-0 border-r-0 lg:border-r border-[rgba(0,0,0,.1)]">
+        <NewsLetterStyles className="w-100 flex flex-col gap-12">
+            <div className="auto w-[85%] lg:w-[70%]  grid grid-cols-1 md:grid-cols-3 items-center justify-center">
+                <div className="p-4 w-100 flex items-start flex-col gap-2 py-4 lg:py-0 border-b lg:border-b-0 border-r-0 lg:border-r border-[rgba(0,0,0,.1)]">
                     <div className="flex items-center gap-2">
                         <FaEdit color={'#C89B42'} fontSize={'30px'} />
                         <h4 className="fs-24 font-bold">Register</h4>
@@ -16,7 +16,7 @@ const NewsLetter = () => {
                         Fill in your personal details in our secure online application.
                     </h5>
                 </div>
-                <div className="p-4  lg:pl-24 pt-4 w-100 flex items-start  flex-col gap-2 border-b lg:border-b-0 pb-0 border-r-0 lg:border-r border-[rgba(0,0,0,.1)]">
+                <div className="p-4  lg:pl-24 w-100 flex items-start  flex-col gap-2 py-4 lg:py-0 border-b lg:border-b-0 pb-0 border-r-0 lg:border-r border-[rgba(0,0,0,.1)]">
                     <div className="flex items-center gap-2">
                         <BsCash fontSize={'30px'} color={'#C89B42'} />
                         <h4 className="fs-24 font-bold">Deposit</h4>
@@ -25,7 +25,7 @@ const NewsLetter = () => {
                         Make a deposit via debit card, wire transfer, eCheck or check
                     </h5>
                 </div>
-                <div className="p-4 lg:pl-24 pt-4 w-100 flex items-start  border-b lg:border-b-0 pb-0 border-r-0 flex-col gap-2 ">
+                <div className="p-4 lg:pl-24 w-100 flex items-start py-4 lg:py-0 border-b lg:border-b-0 border-r-0 flex-col gap-2 ">
                     <div className="flex items-center gap-2">
                         <FaChartLine fontSize={'30px'} color={'#C89B42'} />
                         <h4 className="fs-24 font-bold">
@@ -36,16 +36,26 @@ const NewsLetter = () => {
                     </h5>
                 </div>
             </div>
-            <div className="auto w-[85%] lg:w-[70%] rounded-2xl border py-4 px-12 relative border-[#C89B42]">
-                <h4 className="text-[17px] text-grey font-medium">
-                    Connect with over 5 millions investors in the world’s leading social investment network
-                </h4>
+            <div className="auto w-[85%] mt-12 lg:w-[70%] rounded-2xl flex flex-col gap-2 border py-4 relative border-[#C89B42]">
+                <span className="absolute top-[-8%] left-[10%] p-4 bg-[#C89B42] text-[12px] rounded-full font-medium text-white">OPEN YOUR ACCOUNT</span>
+                <div className="w-90 auto flex flex-col gap-3">
+                    <h4 className="text-[17px] text-grey font-medium">
+                        Connect with over 5 millions investors in the world’s leading social investment network
+                    </h4>
+                    <div className="w-100 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 items-center gap-2">
+                        <input type="text" placeholder='Full Name' className="input" />
+                        <input type="text" placeholder='Email Address' className="input" />
+                        <input type="text" placeholder='Phone Number' className="input" />
+                        <button className="btn btn-2 fs-16 font-bold">Create Account</button>
+                    </div>
+                </div>
             </div>
         </NewsLetterStyles>
     )
 }
 const NewsLetterStyles = styled.div`
    padding: 6rem 0;
+   gap:6rem;
 `
 
 
